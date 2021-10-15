386f42babc648fc6ebab9018d5d610344fbd0588
Ian Miller <imiller@redhat.com>
2021-10-15 16:24:16 -0400

    Verify that the KUBECONFIG env var is set
    Signed-off-by: Ian Miller <imiller@redhat.com>

--- scripts/watch_install.sh
+++ scripts/watch_install.sh
@@ -10,6 +10,11 @@ EOF
     exit 1
 fi
 
+if [[ "${KUBECONFIG}" == "" ]] ; then
+    echo "KUBECONFIG must point to the kubeconfig file for the hub cluster performing the install"
+    exit 1
+fi
+
 # Sometimes the installation CRs are in <name>-installer, check there first
 ns=$1-installer
 events=$( oc get agentclusterinstall -n $ns $1 -o jsonpath="{.status.debugInfo.eventsURL}" 2>/dev/null )
